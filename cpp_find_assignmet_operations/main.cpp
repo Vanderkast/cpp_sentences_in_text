6636b11eb0c92a5fdfb88ddb0e94622c5f36fa0e
Valentine Mazurov <vanderkast@yandex.ru>
2019-06-08 18:44:36 +0300

    Initial Commit

---
+++ cpp_find_assignmet_operations/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_find_assignmet_operations
+//
+//  Created by admin on 08/06/2019.
+//  Copyright © 2019 vanderkast. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
